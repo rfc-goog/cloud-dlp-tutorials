c79bbfe2f0fcbda87333481a6cce8e4e9f886a99
Ruben Fernandez <fernandezruben@google.com.com>
2022-09-29 11:47:10 -0400

    Fixes

--- structured-text-inspect/struct-content-inspect.py
+++ structured-text-inspect/struct-content-inspect.py
@@ -6,25 +6,28 @@ import csv
 import dlp_inspection_conf as config
 
 # Replace with the correct Google Cloud project id
-project_id = "rubenfc-sandbox"
+project_id = "<MY_PROJECT_ID>"
+
 # Read sample data from CSV
 file_name = "sample-data-10lines.csv"
+with open(file_name, encoding="utf-8", newline="") as csvfile:
+  file_content = csv.reader(csvfile, delimiter=",", quotechar='"')
+  csv_header = next(file_content)
+  csv_rows = list(file_content)
 
-with open(file_name, newline='') as csvfile:
-	file_content = csv.reader(csvfile, delimiter=',', quotechar='"')
-	csv_header = next(file_content)
-	csv_rows = list(file_content)
-	print("Header: {}".format(csv_header))
-	print("CSV rows:") 
-	for row in csv_rows:
-		print(', '.join(row))
+  print(f"Header: {csv_header}")
+  print("CSV rows:")
+  for row in csv_rows:
+    print(", ".join(row))
+
 # Instantiate a client.
 dlp_client = google.cloud.dlp_v2.DlpServiceClient()
 # Build table content
 headers = [{"name": val} for val in csv_header]
 rows = []
 for row in csv_rows:
-	rows.append({"values": [{"string_value": cell_val} for cell_val in row]})
+  rows.append({"values": [{"string_value": cell_val} for cell_val in row]})
+
 table = {}
 table["headers"] = headers
 table["rows"] = rows
@@ -32,7 +35,6 @@ item = {"table": table}
 
 # The info types to search for in the content. Required.
 info_types = [{"name": type} for type in config.infoTypeList]
-
 # info_types = [
 # 	{"name": "PERSON_NAME"}, 
 # 	{"name": "STREET_ADDRESS"},
@@ -63,18 +65,20 @@ response = dlp_client.inspect_content(
 )
 # Print out the results.
 if response.result.findings:
-	for finding in response.result.findings:
-		try:
-			print("Quote: {}".format(finding.quote))
-		except AttributeError:
-			pass
-		for location in finding.location.content_locations:
-			print("Original column: {}".format(location.record_location.field_id.name))
-			print("Row index: {}".format(location.record_location.table_location.row_index))
-		print("Info type: {}".format(finding.info_type.name))
+  for finding in response.result.findings:
+    try:
+      print("Quote: {}".format(finding.quote))
+    except AttributeError:
+      pass
+    for location in finding.location.content_locations:
+      print(f"Original column: {location.record_location.field_id.name}")
+      print(f"Row index: {location.record_location.table_location.row_index}")
+
+    print(f"Info type: {finding.info_type.name}")
 		# Convert likelihood value to string respresentation.
-		likelihood = finding.likelihood.name
-		print("Likelihood: {}\n".format(likelihood))
+    likelihood = finding.likelihood.name
+    print(f"Likelihood: {likelihood}\n")
 else:
-	print("No findings.")
+  print("No findings.")
+
 #print(response)
